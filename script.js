34c50a7e16d92c9a4c0ff67c5dd2fe974f745830
KNAthari <143450459+KNAthari@users.noreply.github.com>
2023-10-10 11:57:22 -0500

    2.2.5-mark-complete

---
+++ script.js
@@ -0,0 +1,8 @@
+$(document).ready(function(){
+    console.log("ready");
+    $("td:nth-child(3)").each(function(){
+        if($(this).text() == "Complete"){
+            $(this).css("background-color", "lightgreen");
+        }
+    })
+});
